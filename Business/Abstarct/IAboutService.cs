3dd1eed31cee26cfdd4715f1eea58856d21363e5
IbrahimFeteliyev <feteliyevibrahim@gmail.com>
2024-02-29 17:10:53 +0400

    first_commit

---
+++ Business/Abstarct/IAboutService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.DTOs.AboutDTOs;
+using Entities.DTOs.CategoryDTOs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstarct
+{
+    public interface IAboutService
+    {
+        Task<Core.Utilities.Results.Abstract.IResult> AddAboutByLanguageAsync(AboutAddDTO aboutAddDTO);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-C642ENA\SQLEXPRESS;Database=AppDB;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        }
+        public DbSet<Slider> Sliders { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<WorkSchedule> WorkSchedules { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryLanguage> CategoryLanguages { get; set; }
+
+        public DbSet<ShortInfo> ShortInfos { get; set; }
+        public DbSet<ShortInfoLanguage> ShortInfoLanguages { get; set; }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AboutLanguage> AboutLanguages { get; set; }
+
+    }
+}
